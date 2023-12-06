578cf303b6fba34f5cdf3db0da0d85dd7b229c5e
Alex <sirbu.alexandr.dev@gmail.com>
2023-12-06 13:06:53 +0200

    modified commit

--- script.js
+++ script.js
@@ -24,28 +24,25 @@ const activeBox = document.querySelector('.active_box')
 const activeBox2 = document.querySelector('.active_box2')
 const activeBox3 = document.querySelector('.active_box3')
 
+const numbers = document.querySelectorAll('.animated-heading');
+const input = document.getElementById('input');
 
+let index = 0;
 
 const images = [
   './img/visaLogo_st.png',
   './img/discoverLogo_st.png',
   './img/masterCard_st.png',
 ];
-values4by4 = '';
-let cardNumberValue = Array(16).fill('#').join('');
-for (let i = 0; i < cardNumberValue.length; i++) {
-  if (i % 4 == 0 && i > 0) values4by4 = values4by4.concat('  ');
-  values4by4 = values4by4.concat(cardNumberValue[i]);
-};
+
+
 let ind = 0;
 const change = () => {
   logo.src = images[ind];
   ind > 1 ? ind = 0 : ind++;
 };
 window.onload = function () {
-  cardNumber.value = values4by4
   setInterval(change, 8000)
-
 };
 
 cardInputNumber.addEventListener('click', () => {
@@ -55,37 +52,12 @@ cardInputNumber.addEventListener('click', () => {
   activeBox3.classList.toggle('active_box_animation')
 });
 
-// cardInputNumber.addEventListener('input', (e) => {
 
-//   let fullNumbers = e.target.value.replace(/\D/g, '');
-//   let numbers4by4 = '';
-//   e.target.value = fullNumbers;
-//   if (fullNumbers.length > 16) {
-//     // e.preventDefault()
-//     e.target.value = fullNumbers.substring(0, 16)
-//     return
-//   };
-
-//   fullNumbers = fullNumbers.split(' ').join('');
-
-//   for (let i = 0; i < fullNumbers.length; i++) {
-//     if (i % 4 == 0 && i > 0) numbers4by4 = numbers4by4.concat('  ');
-//     numbers4by4 = numbers4by4.concat(fullNumbers[i]);
-//   }
-//   cardNumber.value = numbers4by4 + values4by4.slice(numbers4by4.length, values4by4.length);
-//   cardNumber.value[1].style.color = 'red'
-// });
-
-// proba
-
-const numbers = document.querySelectorAll('.animated-heading');
-const input = document.getElementById('input');
-
-let index = 0;
 cardInputNumber.addEventListener('input', (e) => {
 
   let fullNumbers = e.target.value.replace(/\D/g, '');
   e.target.value = fullNumbers;
+
   if (index > 16) {
     e.preventDefault()
     e.target.value = fullNumbers.substring(0, 16)
@@ -93,13 +65,11 @@ cardInputNumber.addEventListener('input', (e) => {
     return
   };
   for (let i = 0; i < numbers.length; i++) {
-
     numbers[i].innerHTML = cardInputNumber.value[i]
     if (numbers[i].innerHTML === 'undefined') {
       numbers[i].innerHTML = '#'
     }
   }
-
 
   let elem = document.getElementById(`${index}`)
   const spanUnu = document.createElement('span')
@@ -108,86 +78,78 @@ cardInputNumber.addEventListener('input', (e) => {
   elem.appendChild(spanUnu)
   elem.classList.remove('animation_down')
   elem.classList.add('animation')
-})
+});
+
 
 cardInputNumber.addEventListener('keydown', (e) => {
-  if (e.keyCode == 8) {
-    let elem = document.getElementById(`${index}`)
-    elem.removeChild(document.getElementById(`span_${index}`))
-    elem.classList.remove('animation')
-    elem.classList.add('animation_down')
-    index--
-    console.log(index)
-  }
-  else {
+  if (e.keyCode < 48 || e.keyCode > 57) {
+    index
+    if (e.keyCode == 8) {
+      let elem = document.getElementById(`${index}`);
+      elem.removeChild(document.getElementById(`span_${index}`));
+      elem.classList.remove('animation');
+      elem.classList.add('animation_down');
+      index--
+    }
+  } else {
     index++
-    console.log(index)
   }
-})
+});
 
 cardInputName.addEventListener('click', () => {
   activeBox.classList.add('box_hidden');
-
   activeBox2.classList.remove('box_hidden');
-  activeBox3.classList.add('box_hidden')
+  activeBox3.classList.add('box_hidden');
 })
 
 cardInputName.addEventListener('input', (e) => {
-
+  let fullLetters = e.target.value.replace(/\d+|^\s+$/g, '');
+  e.target.value = fullLetters;
   cardName.value = (e.target.value).toUpperCase();
 });
-
 
 cardInputMonth.addEventListener('click', () => {
   activeBox2.classList.add('box_hidden');
   activeBox3.classList.remove('box_hidden')
   setTimeout(() => {
     if (a)
-      activeBox3.classList.add('active_box_animation')
-    // activeBox3.style.animation = 'none'
-  }, 500)
-})
+      activeBox3.classList.add('active_box_animation');
+  }, 500);
+});
 
 cardInputYear.addEventListener('click', () => {
   activeBox2.classList.add('box_hidden');
-  activeBox3.classList.remove('box_hidden')
+  activeBox3.classList.remove('box_hidden');
   setTimeout(() => {
-    activeBox3.classList.add('active_box_animation')
-    // activeBox3.style.animation = 'none'
-  }, 500)
-})
+    activeBox3.classList.add('active_box_animation');
+
+  }, 500);
+});
 cardInputMonth.addEventListener('input', (e) => {
   cardMonth.value = e.target.value
-})
+});
 
 cardInputYear.addEventListener('input', (e) => {
   cardYear.value = e.target.value
-})
+});
 
 cardInputCvc.onblur = () => {
   flip.classList.toggle("is-flipped");
   cardFace.classList.remove('hidden');
   cardBack.classList.toggle('hidden');
-  activeBox3.classList.add('box_hidden')
-  // cardFace.style.display = 'block';
-  // cardBack.style.display = 'none';
-
-}
+  activeBox3.classList.add('box_hidden');
+};
 
 cardInputCvc.onfocus = (e) => {
   flip.classList.toggle("is-flipped");
   cardFace.classList.toggle('hidden');
   cardBack.classList.toggle('hidden');
-  // activeBox3.classList.toggle('active_box_animation')
-  // cardFace.style.display = 'none';
-  // cardBack.style.display = 'block';
   cardInputCvc.addEventListener('input', (e) => {
     let onlyNumbers = e.target.value.replace(/\D/g, '');
     e.target.value = onlyNumbers;
-    cardCvc.value = e.target.value
-    console.log(e.target.value)
-  })
-}
+    cardCvc.value = e.target.value;;
+  });
+};
 
 
 
